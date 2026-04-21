bdc757a1d999046a5fe153c7e278c47fa2067cb4
marveycrypton <arayinka15@gmail.com>
2026-04-21 16:48:51 +0100

    Updated server for deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/loginDB")
+mongoose.connect("mongodb+srv://arayinka15_db_user:NjlanibNv02ReTZc@cluster0.v1spvol.mongodb.net/?appName=Cluster0")
 .then(() => console.log("DB connected"))
 .catch(err => console.log(err));
 
@@ -65,4 +65,11 @@ app.listen(5000, () => {
 app.post("/signup", (req, res) => {
   console.log("🔥 signup hit");
   res.json({ message: "ok" });
+
+  const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
+
 });
